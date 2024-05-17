04975cba92f346d3138e33239927b365608d933d
aphan4 <aphan4@uw.edu>
2024-05-17 15:20:53 -0700

    hi

---
+++ Table of Summary Info.R
@@ -0,0 +1,12 @@
+# Audrey Phan
+# Table of Summary Information
+
+# Load packages
+library(tidyverse)
+library(dplyr)
+
+# Load Data
+
+student_study_performance <- read.csv("Downloads/study_performance.csv")
+View(student_study_performance)
+
