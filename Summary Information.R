850f73b08a026d6885eb5cd5ea5dfd5e247a3ae8
aphan4 <aphan4@uw.edu>
2024-05-17 13:11:01 -0700

    hi

--- Summary Information.R
+++ Summary Information.R
@@ -13,4 +13,7 @@ View(student_study_performance)
 
 # Pull the student with the lowest test scores
 student_lowest_scores <- student_study_performance %>%
-  filter(math_score )
+  filter(math_score == max(math_score)) %>%
+  filter(reading_score == max(reading_score)) %>%
+  filter(writing_score == max(writing_score)) %>%
+  
